eccc98185fb7c2af846a7d57fc3a87b8d60cb2fb
ryan fackett <ryan@foghornconsulting.com>
2017-04-26 15:41:46 -0700

    removed incorrect test switch

--- dotnet.sh
+++ dotnet.sh
@@ -3,7 +3,7 @@
 cd /IdentityServer4
 dotnet --info
 dotnet restore
-dotnet test --project ./test/IdentityServer.IntegrationTests/IdentityServer.IntegrationTests.csproj
-dotnet test --project ./test/IdentityServer.UnitTests/IdentityServer.UnitTests.csproj
+dotnet test ./test/IdentityServer.IntegrationTests/IdentityServer.IntegrationTests.csproj
+dotnet test ./test/IdentityServer.UnitTests/IdentityServer.UnitTests.csproj
 dotnet run --proejct ./src/IdentityServer4/IdentityServer4.csproj
 exit
