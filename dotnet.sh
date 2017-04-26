460f3c8fabf296df2f2df0cfd34e375474249017
ryan fackett <ryan@foghornconsulting.com>
2017-04-26 15:36:43 -0700

    changed run to test

--- dotnet.sh
+++ dotnet.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 cd /IdentityServer4
+dotnet --info
 dotnet restore
-dotnet run --project ./test/IdentityServer.IntegrationTests/IdentityServer.IntegrationTests.csproj
-dotnet run --project ./test/IdentityServer.UnitTests/IdentityServer.UnitTests.csproj
+dotnet test --project ./test/IdentityServer.IntegrationTests/IdentityServer.IntegrationTests.csproj
+dotnet test --project ./test/IdentityServer.UnitTests/IdentityServer.UnitTests.csproj
+dotnet run --proejct ./src/IdentityServer4/IdentityServer4.csproj
 exit
